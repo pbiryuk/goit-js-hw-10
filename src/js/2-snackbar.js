b30980b76bb4094c2f1ab591c0b7cb482633d975
pbiryuk <biryuk.p.biryuk@gmail.com>
2025-05-20 01:17:12 +0300

    test1

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,39 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        title: '✅',
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        timeout: 3000,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: '❌',
+        message: `Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        timeout: 3000,
+      });
+    });
+
+  form.reset(); 
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      state === 'fulfilled' ? resolve(delay) : reject(delay);
+    }, delay);
+  });
+}
